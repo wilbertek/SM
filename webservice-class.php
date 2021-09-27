5016ba0d640999f5c6500a8c644ba3d0926778ef
root <root@epic-wright.54-198-61-187.plesk.page>
2021-09-27 22:55:43 +0000

    cambios en decode.php

--- webservice-class.php
+++ webservice-class.php
@@ -27,7 +27,7 @@ class WebserviceBoxito {
 		        switch($json->metodo) {
                    
 				case 'Noticias':
-					$Noti = array();
+					/*$Noti = array();
 					$consulta="SELECT * from Noticias ";
 					$resultado = mysqli_query( $con, $consulta ) or die ( "Error: 001"); 
 					while ($columna = mysqli_fetch_array( $resultado ))
@@ -35,32 +35,67 @@ class WebserviceBoxito {
 						$Noti[]= $columna["Title"];
 					}
 					
-					$response=$Noti;
+					$response=$Noti;*/
 				
 				break;
 		
 				case 'addReport':
 						
 						$ok = $this->add_report($json);						
-						$response = array('metodo' => $this->metodo,'status' => $ok);			
+						$response = array('metodo' => $this->metodo,'status' => $ok ,'mensaje' => '');			
 
 				break;
 						
 				case 'addUser':
 						
-						$ok = $this->add_user($json);
-						$response = array('metodo' => $this->metodo,'status' => $ok);			
+						$v= $this->validate_caorreo($json);
+						
+						if($v == 0)
+						{
+							$ok = $this->add_user($json);
+							$response = array('metodo' => $this->metodo,'status' => $ok ,'mensaje' => '');
+						}
+						else
+						{
+							$response = array('metodo' => $this->metodo,'status' => 'error' ,'mensaje' => 'El correo ya existe');
+						}
+						
+									
 				break;
 				
 				case 'getReports':
 						
-						echo '{"metodo":"getReports", "status": "successfully","Reportes":[';
+						echo '{"metodo":"getReports", "status": "successfully","mensaje":"","reportes":[';
 						$ok = $this->get_rep($json);
 						echo ']}';
 						die();
 				
 				break;
 				
+				case 'loginUsers':
+						
+						echo '{"metodo":"loginUsers", "status": "successfully","mensaje":"", "usuario":[';
+						$ok = $this->Login($json);
+						echo ']}';
+						die();
+				
+				break;
+				
+				case 'updateUser':
+						
+						$ok = $this->update_user($json);
+						$response = array('metodo' => $this->metodo,'status' => $ok ,'mensaje' => '');			
+				break;
+				
+				case 'getUser':
+						
+						echo '{"metodo":"getUser", "status": "successfully","mensaje":"","usuario":[';
+						$ok = $this->get_user($json);
+						echo ']}';
+						die();
+						
+				break;
+						
 				default:
 				$this->error = 'Este metodo no es valido';
 		        
@@ -77,9 +112,32 @@ class WebserviceBoxito {
 		echo json_encode($response);
     }
     
+	function validate_caorreo($json)
+	{
+		$exist =0;
+		$con = mysqli_connect('localhost', 'admini', '5Chwh83~','admin_K');
+		if (!$con) 
+		{
+			  die("Connection failed: " . mysqli_connect_error());
+		}
+	
+		$sqlc = "Select * from Usuarios where correo= '".$json->usuario->correo."'";
+		$resultado = mysqli_query( $con, $sqlc ) or die ( "Error: 001-metod:val_e"); 
+		while ($columna = mysqli_fetch_array( $resultado ))
+		{
+			if ($columna["correo"]  == $json->usuario->correo)
+			{
+				$exist = 1 ;
+			}
+		}
+		
+		return $exist;
+		
+	}
   
     function add_report($json) 
 	{
+		//print_r($json->reporte->evidencias);
 		
 		$val="";
 		if(isset($json->reporte))
@@ -120,23 +178,26 @@ class WebserviceBoxito {
 				  die("Connection failed: " . mysqli_connect_error());
 				}
 
-				$sql = "INSERT INTO Usuarios (Nombre,a_paterno,a_materno,f_nacimiento,calle,numero,cruzamientos,colonia,correo,telefono,password) values ('".$json->usuario->Nombre."','".$json->usuario->a_paterno."','".$json->usuario->a_materno."','".$json->usuario->f_nacimiento."','".$json->usuario->calle."','".$json->usuario->numero."','".$json->usuario->cruzamientos."','".$json->usuario->colonia."','".$json->usuario->correo."','".$json->usuario->telefono."','".$json->usuario->password."')";
+				$sql = "INSERT INTO Usuarios (Nombre,a_paterno,a_materno,f_nacimiento,calle,numero,cruzamientos,colonia,correo,telefono,password) values ('".$json->usuario->nombre."','".$json->usuario->a_paterno."','".$json->usuario->a_materno."','".$json->usuario->f_nacimiento."','".$json->usuario->calle."','".$json->usuario->numero."','".$json->usuario->cruzamientos."','".$json->usuario->colonia."','".$json->usuario->correo."','".$json->usuario->telefono."','".$json->usuario->password."')";
 				
-				if (mysqli_query($con, $sql)) {
-				  $val = "successfully";
-				} else {
-				  $val = "Error: " . $sql . " " . mysqli_error($con);
+					if (mysqli_query($con, $sql)) 
+					{
+				  		$val = "successfully";
+					} 
+					else 
+					{
+				  		$val = "Error: " . $sql . " " . mysqli_error($con);
+					}
 				}
-				
-			}
-		
 		return $val;
+	}
+		
+		
 		    
-    }
 
 	function get_rep($json)
 	{
-		$myObj = new stdClass();
+
 		if(isset($json->usuario))
 		{
 			$con = mysqli_connect('localhost', 'admini', '5Chwh83~','admin_K');
@@ -152,8 +213,87 @@ class WebserviceBoxito {
 			while ($columna = mysqli_fetch_array( $resultado ))
 				{
 					$c++;
-					echo '{"categoria":"' .$columna["categoria"]. '","comentario":"' .$columna["comentario"].'","calle": "'.$columna["calle"].'","numero": "'.$columna["cruzamientos"].'", "colonia": "'.$columna["colonia"].'", "referencia": "'.$columna["referencia"].'","coordenadas": "'.$columna["coordenadas"].'", "evidencias": "'.$columna["evidencias"].'", "fecha_inicio": "'.$columna['fecha_inicio'].'", "status": "'.$columna["status"].'" }';
+					echo '{"categoria":"'.$columna["categoria"]. '","comentario":"'.$columna["comentario"].'","calle": "'.$columna["calle"].'","numero": "'.$columna["numero"].'","cruzamientos": "'.$columna["cruzamientos"].'", "colonia": "'.$columna["colonia"].'", "referencia": "'.$columna["referencia"].'","coordenadas": "'.$columna["coordenadas"].'", "evidencias": "'.$columna["evidencias"].'", "fecha_inicio": "'.$columna['fecha_inicio'].'", "status": "'.$columna["status"].'"}';
 					
+					if($c < $row_cnt)
+					{
+						echo ',';
+					}
+				}
+		}
+		
+	}
+	
+	function Login($json)
+	{
+		if(isset($json->usuario))
+		{
+			$con = mysqli_connect('localhost', 'admini', '5Chwh83~','admin_K');
+				if (!$con) 
+				{
+				  die("Connection failed: " . mysqli_connect_error());
+				}
+			$jpas = md5($json->usuario->password);
+			$consulta="SELECT * from Usuarios where correo =  '".$json->usuario->correo."'";
+			$resultado = mysqli_query( $con, $consulta ) or die ( "Error: 001"); 
+			while ($columna = mysqli_fetch_array( $resultado ))
+				{
+					if (md5($columna["password"]) == $jpas && $columna["correo"] == $json->usuario->correo)
+					{
+						echo '{"idUsuario":"' .$columna["idUsuario"]. '","nombre":"' .$columna["Nombre"]. '","a_paterno":"' .$columna["a_paterno"].'"}';
+					}
+				}
+		}
+	
+	}
+	
+  function update_user($json) 
+	{
+		
+		$val="";
+		if(isset($json->usuario))
+			{
+				
+				$con = mysqli_connect('localhost', 'admini', '5Chwh83~','admin_K');
+				if (!$con) 
+				{
+				  die("Connection failed: " . mysqli_connect_error());
+				}
+			
+			$sql = "update Usuarios set Nombre ='".$json->usuario->nombre."', a_materno= '".$json->usuario->a_paterno."', a_materno = '".$json->usuario->a_materno."', f_nacimiento = '".$json->usuario->f_nacimiento."', calle = '".$json->usuario->calle."', numero='".$json->usuario->numero."', cruzamientos='".$json->usuario->cruzamientos."', colonia = '".$json->usuario->colonia."', correo= '".$json->usuario->correo."', telefono= '".$json->usuario->telefono."', password = '".$json->usuario->password."'  where idUsuario = ".$json->usuario->idUsuario." ";
+				
+				if (mysqli_query($con, $sql)) {
+				  $val = "successfully";
+				} else {
+				  $val = "Error: " . $sql . " " . mysqli_error($con);
+				}
+				
+			}
+		
+		return $val;
+		    
+    }
+	
+	function get_user($json)
+	{
+		
+		if(isset($json->usuario))
+		{
+			$con = mysqli_connect('localhost', 'admini', '5Chwh83~','admin_K');
+				if (!$con) 
+				{
+				  die("Connection failed: " . mysqli_connect_error());
+				}
+			
+			$consulta="SELECT * from Usuarios where idUsuario =  ".$json->usuario->idUsuario."";
+			$resultado = mysqli_query( $con, $consulta ) or die ( "Error: 001"); 
+			$row_cnt = $resultado->num_rows;
+			$c = 0;
+			while ($columna = mysqli_fetch_array( $resultado ))
+				{
+					$c++;
+					echo '{"idUsuario":"' .$columna["idUsuario"]. '","nombre":"' .$columna["Nombre"]. '","a_paterno":"' .$columna["a_paterno"].'", "a_materno":"'.$columna["a_materno"].'", "f_nacimiento":"'.$columna["f_nacimiento"].'", "calle":"'.$columna["calle"].'", "numero":"'.$columna["numero"].'", "cruzamientos":"'.$columna["cruzamientos"].'", "colonia":"'.$columna["colonia"].'", "correo":"'.$columna["correo"].'", "telefono":"'.$columna["telefono"].'", "password":"'.$columna["password"].'" }';
+	
 					if($c < $row_cnt)
 					{
 						echo ',';
